037f2c52be655d4732394c2b669a3e66bd9d7b9a
Vitaliy Kalachev <102898730+vitaliykalachev@users.noreply.github.com>
2022-07-26 19:01:22 +0700

    worked version python 3.10

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Recipes
+
 # Create your views here.
 
 def index(request):
+    recipes = Recipes.objects,all().values()
     template = loader.get_template('home.html')
+    
+    context = {
+        'recipes': recipes,
+    }
+    
     return HttpResponse(template.render({}, request))
+    
+    
